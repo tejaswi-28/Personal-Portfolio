9d0a6880c2c80cd4228d4616711f74980028e454
Tejaswi Gaikwad <103772947+tejaswi-28@users.noreply.github.com>
2024-02-13 19:33:47 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+var typed = new Typed(".text",{
+    strings: ["Web Developer", "Java Developer"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+});
+
+function myFunction() {
+    var x = document.getElementById("myDiv");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
